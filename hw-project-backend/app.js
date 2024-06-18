233489dfc77ac352a818f18981ff7716b0f22851
leopokoyo <leopokoyo@hotmail.com>
2024-06-18 17:12:00 +0200

    Initial Routes added API ENDPOINTS CHANGED: - countries.js
    ADDED CORS + BASE CONFIG

--- hw-project-backend/app.js
+++ hw-project-backend/app.js
@@ -1,26 +1,25 @@
-// app.js
-
 const express = require('express');
 const countriesRouter = require('./routes/countries');
 const racesRouter = require('./routes/races');
 const raceInfoRouter = require('./routes/raceInfo');
-const status = require('./routes/status')
+const statusRouter = require('./routes/status');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON bodies
+// Middleware
 app.use(express.json());
+app.use(cors());  // Basic CORS configuration
 
-// Mount route modules
+// Mount routes
 app.use('/api/countries', countriesRouter);
 app.use('/api/races', racesRouter);
 app.use('/api/race-info', raceInfoRouter);
-app.use('/api/status', status);
+app.use('/api/status', statusRouter);
 
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
