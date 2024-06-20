c0b9b51f63aa7159f4268f9ed72eccc8abf6e694
leopokoyo <leopokoyo@hotmail.com>
2024-06-20 20:58:58 +0200

    Routes Fixed - Changes to the database made
    - Style Fixes made in all routes
    
    Routes added
    
    - API Endpoints added to temp.js

--- hw-project-backend/routes/temp.js
+++ hw-project-backend/routes/temp.js
@@ -3,7 +3,7 @@ const db = require('../db/db');
 
 const router = express.Router();
 
-router.put('/:p1/:p2/:p3/:p4/:raceSeed', async (req, res) => {
+router.put('/participants/:p1/:p2/:p3/:p4/:raceSeed', async (req, res) => {
     const {p1, p2, p3, p4, raceSeed} = req.params;
 
     try {
@@ -23,7 +23,7 @@ router.put('/:p1/:p2/:p3/:p4/:raceSeed', async (req, res) => {
 });
 module.exports = router;
 
-router.get('/', async (req, res) => {
+router.get('/participants/', async (req, res) => {
     try {
         const countries_data = await db.any(
             `
@@ -34,6 +34,38 @@ router.get('/', async (req, res) => {
         res.json(countries_data);
     } catch (error) {
         console.error('Error fetching countries data:', error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+});
+
+router.put('/betting/:p1/:p2/:p3/:p4/', async (req, res) => {
+    const {p1, p2, p3, p4} = req.params;
+
+    try {
+        await db.tx(async t => {
+            const updateQuery =`
+                UPDATE big_mac_data.temp_betting
+                SET p1 = $1, p2 = $2, p3 =$3, p4=$4
+            `;
+            await t.none(updateQuery, [p1,p2,p3,p4])
+            res.status(200).json({message: 'Row updated successfully'});
+        });
+    } catch (error) {
+        console.error('Error updating temp betting:', error);
+        res.status(500).json({message:'Internal Server Error'});
+    }
+})
+
+router.get('/betting/', async (req, res) => {
+    try {
+        const betting_data = await db.any(
+            `
+            SELECT * 
+            FROM big_mac_data.temp_betting 
+        `);
+        res.json(betting_data);
+    } catch (error) {
+        console.error('Error fetching betting data:', error);
         res.status(500).json({message: 'Internal server error'});
     }
 });
